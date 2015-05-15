0a74be64c1c7570cef7800dae212acb9b3f379cc
Nico Zhi <tczdf123@hotmail.com>
2015-05-15 18:53:42 +0800

    Mino Base

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ Tetris.prototype = {
     height: DEFAULT_HEIGHT,
 
     init: function (dom) {
+        var that = this;
         this.matrix = this._createMatrix(this.width, this.height);
         if (dom) {
             this.display = new TetrisDisplay(dom);
@@ -29,6 +30,16 @@ Tetris.prototype = {
                 'matrix': this.matrix
             });
         }
+        setInterval(function () {
+            if (that.currentMino) {
+                that.currentMino.moveDown();
+            } else {
+                that.currentMino = new Tetromino();
+                that.currentMino.enterMatrx(5);
+            }
+            that.render();
+            console.log('done.');
+        }, 1000);
     },
 
     //create an empty matrix
@@ -43,27 +54,173 @@ Tetris.prototype = {
             m.push(row);
         }
         //debug
-        m[18][8] = 1;
-        m[18][9] = 1;
-        m[19][8] = 1;
-        m[19][9] = 1;
+        // m[0][8] = 1;
+        // m[0][9] = 1;
+        // m[1][8] = 1;
+        // m[1][9] = 1;
         return m;
     },
 
     start: function () {},
 
-    render: function () {}
+    render: function () {
+        var currentMatrix = [], row, currentMinoPos, currentMinoType;
+
+        for (var r = 0; r < this.matrix.length; r++) {
+            row = [];
+            for (var c = 0; c < this.matrix[r].length; c++) {
+                row.push(this.matrix[r][c]);
+            }
+            currentMatrix.push(row);
+        }
+
+        if (this.currentMino) {
+            currentMinoType = 1; // temporary 1 used
+            currentMinoPos = this.currentMino.getCurrentPosition();
+            for (var i = 0; i < currentMinoPos.length; i++) {
+                currentMatrix[currentMinoPos[i][1]][currentMinoPos[i][0]] = currentMinoType;
+            }
+        }
+        this.display.update({
+            'matrix': currentMatrix
+        });
+    }
 };
 
 window.Tetris = Tetris;
 
 //Tetromino Object
-var Tetromino = function (argument) {};
+var Tetromino = function (difficulty, index, status) {
+    this.init(difficulty);
+};
 
 Tetromino.prototype = {
     constructor: Tetromino,
-    init: function () {},
-    enterMatrx: function () {}
+    x: null,
+    y: null,
+    difficulty: null,
+    index: null,
+    status: null,
+    init: function (difficulty, index, status) {
+        if (!difficulty) difficulty = "demo";
+        if (!index && index != 0) index = 0;
+        if (!status && status != 0) status = 0;
+        this.difficulty = difficulty;
+        this.index = index;
+        this.status = status;
+    },
+
+    enterMatrx: function (x, y) {
+        if (!x) {
+            this.x = 0;
+            console.info('enterMatrx: x is not defined.');
+        } else {
+            this.x = x;
+        }        
+        if (y) {
+            this.y = y;
+            console.info('enterMatrx: specified y set.');
+        } else {
+            this.y = 0;
+        }
+        console.log(this.getCurrentPosition());
+    },
+
+    //fixPosition: function () {},
+
+    moveDown: function () {
+        this.y = this.y + 1;
+    },
+
+    moveLeft: function () {
+        this.x = this.x - 1;
+    },
+
+    moveRight: function () {
+        this.x = this.x + 1;
+    },
+
+    rotate: function () {},
+
+    getCurrentPosition: function () {
+        if (this.x === null || this.y === null) {
+            return null;
+        }
+        var arr = [],
+            baseArr = this.shapes[this.difficulty][this.index][this.status];
+        for (var i = 0; i < baseArr.length; i++) {
+            arr.push([]);
+            arr[i][0] = baseArr[i][0] + this.x;
+            arr[i][1] = baseArr[i][1] + this.y;
+        }
+        return arr;
+    },
+
+    getMoveDownPosition: function () {
+        if (this.x === null || this.y === null) {
+            return null;
+        }
+        var arr = [],
+            x = this.x,
+            y = this.y + 1,
+            baseArr = this.shapes[this.difficulty][this.index][this.status];
+        for (var i = 0; i < baseArr.length; i++) {
+            arr.push([]);
+            arr[i][0] = baseArr[i][0] + x;
+            arr[i][1] = baseArr[i][i] + y;
+        }
+        return arr;
+    },
+    
+    getMoveLeftPosition: function () {
+        if (this.x === null || this.y === null) {
+            return null;
+        }
+        var arr = [],
+            x = this.x - 1,
+            y = this.y,
+            baseArr = this.shapes[this.difficulty][this.index][this.status];
+        for (var i = 0; i < baseArr.length; i++) {
+            arr.push([]);
+            arr[i][0] = baseArr[i][0] + x;
+            arr[i][1] = baseArr[i][i] + y;
+        }
+        return arr;
+    },
+    
+    getMoveRightPosition: function () {
+        if (this.x === null || this.y === null) {
+            return null;
+        }
+        var arr = [],
+            x = this.x + 1,
+            y = this.y,
+            baseArr = this.shapes[this.difficulty][this.index][this.status];
+        for (var i = 0; i < baseArr.length; i++) {
+            arr.push([]);
+            arr[i][0] = baseArr[i][0] + x;
+            arr[i][1] = baseArr[i][i] + y;
+        }
+        return arr;
+    },
+
+    getRotatePosition: function () {}
+};
+
+//definite different types of shapes
+Tetromino.prototype.shapes = {
+    "demo": 
+    [//diff
+        [
+            [[0, 0], [1, 0], [1, 1], [1, 2]]
+        ],
+        [
+            [[0, 0], [0, 1], [1, 0], [1, 1]]
+        ],
+        [//index
+            [[0, 0]] //status //points [x, y]
+        ]
+    ]
 };
 
 //Tetris Display Object
@@ -95,7 +252,7 @@ TetrisDisplay.prototype = {
                         row += cell;
                     }
                     row += '<br />';
-                    m = row + m;
+                    m = m + row;
                 }
             } catch (e) {
                 m = "";
